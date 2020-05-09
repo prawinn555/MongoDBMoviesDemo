faa2e7186a24e3cf947d92539b36044081445387
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-09 15:03:28 +0200

    welcome page

--- routes/router.js
+++ routes/router.js
@@ -7,5 +7,17 @@ router.get('/api/a',  require('../api/a.js'));
 router.get('/api/b',  require('../api/b.js'));
 router.get('/api/c',  require('../api/c.js'));
 
+let welcome = async (req, res) => {
+  console.info("welcome  ", req, res);
+  try {
+    res.send(`Welcome`);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+router.get("/", welcome);
+router.get("/api/welcome", welcome);
+
 
 module.exports = router;
