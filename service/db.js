e98bcd6d17d0763c72978a258efeeb3cec6bbf29
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-10 13:00:36 +0200

    initdb

--- service/db.js
+++ service/db.js
@@ -5,39 +5,40 @@ var exists;
 var sqlite3 ;
 var db;
 
-try {
-  exists = fs.existsSync(dbFile);
-  sqlite3 = require('sqlite3').verbose();
-  db =  new sqlite3.Database(dbFile); 
-
-	db.serialize(function(){
-	  if (!exists) {
-		let sql = `CREATE TABLE mydata (id TEXT, type TEXT, description TEXT, content TEXT)`;
-	    db.run(sql);
-	    console.log('Runing SQL', sql);
-	    
-	    sql = `INSERT INTO mydata (id, type, description) VALUES 
-	          ("1", "test", "test"), 
-	          ("2", "test", "test"),
-	          ("3", "test", "test")
-	          `;
-	    db.serialize(function() {
-	      db.run(sql);
-	    });
-	  } else {
-	    console.log('Database ready to go!');
-	    console.log('Its content is the following : ');
-	    db.each('SELECT * from mydata', function(err, row) {
-	      if ( row ) {
-	        console.log('record:', row); 
-	      }
-	    });
-	  }
-	});
-} catch(e) {
-  console.log('error init DB', e);
+var initDb = () => {
+	try {
+	  exists = fs.existsSync(dbFile);
+	  sqlite3 = require('sqlite3').verbose();
+	  db =  new sqlite3.Database(dbFile); 
+	
+		db.serialize(function(){
+		  if (!exists) {
+			let sql = `CREATE TABLE mydata (id TEXT, type TEXT, description TEXT, content TEXT)`;
+		    db.run(sql);
+		    console.log('Running SQL', sql);
+		    
+		    sql = `INSERT INTO mydata (id, type, description) VALUES 
+		          ("1", "test", "test"), 
+		          ("2", "test", "test"),
+		          ("3", "test", "test")
+		          `;
+		    db.serialize(function() {
+		      db.run(sql);
+		    });
+		  } else {
+		    console.log('Database ready to go!');
+		    console.log('Its content is the following : ');
+		    db.each('SELECT * from mydata', function(err, row) {
+		      if ( row ) {
+		        console.log('record:', row); 
+		      }
+		    });
+		  }
+		});
+	} catch(e) {
+	  console.log('error init DB', e);
+	}
 }
-
 
 
 // Export the model
