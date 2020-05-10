b59763000496cedf56c8e46d5ea27eab8bcdd6a2
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-10 13:01:54 +0200

    listservice

--- api/index.js
+++ api/index.js
@@ -3,8 +3,7 @@ module.exports = async (req, res) => {
   console.log('call index v2');
   try {
     var utils = require('../service/utils');
-	res.send('hello');
-    // res.send(`${utils.serviceListHTML()}`);
+    res.send(`${utils.serviceListHTML()}`);
   } catch(e)  {
 	console.error(e);
 	res.send('ERR '+e);
