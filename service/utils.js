f9edf30b9a1ffc07c5cdb2822dbdd46afdfb8b39
lapin <lapin@psriplakich-Pc.npif.local>
2020-05-11 13:15:01 +0200

    allow origin

--- service/utils.js
+++ service/utils.js
@@ -14,9 +14,9 @@ function sendOk(response, msg) {
 
 
 function prepareHeader(res) { 
-  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  res.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE, HEAD");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE, HEAD");
 }
 
 const path = require('path');
